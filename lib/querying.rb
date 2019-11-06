4b05394d35a5e02276bd9d642df95513a5a48cbc
Victoria Hale <vic.hale74@gmail.com>
2019-11-06 03:40:57 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,7 +22,9 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM authors 
   JOIN series 
-  ON "
+  ON authors.id = series.author_id
+  JOIN series 
+  ON subgenres.id = series.subgenre_id" 
 end
 
 def select_series_title_with_most_human_characters
