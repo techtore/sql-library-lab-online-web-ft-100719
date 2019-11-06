4248299e6c7cd7b81629bddbd7bbff4bcd8b1f08
Victoria Hale <vic.hale74@gmail.com>
2019-11-06 03:26:11 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,8 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT value, count FROM species 
+  ORDER BY "
 end
 
 def select_name_and_series_subgenres_of_authors
