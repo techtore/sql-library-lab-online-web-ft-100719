058cc1198e5413636de5429571f439efd9bbe190
Victoria Hale <vic.hale74@gmail.com>
2019-11-06 03:45:56 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,9 +28,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT title, COUNT(characters.species) FROM series
-   JOIN characters 
-   ON "
+  "S"
 end
 
 def select_character_names_and_number_of_books_they_are_in
