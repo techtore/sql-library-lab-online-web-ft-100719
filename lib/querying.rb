f3db4aa65ada8773dcad0e368901bc4f5a6ae203
Victoria Hale <vic.hale74@gmail.com>
2019-11-06 03:43:01 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,7 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM authors 
   JOIN series 
   ON authors.id = series.author_id
-  JOIN series 
+  JOIN subgenre 
   ON subgenres.id = series.subgenre_id;" 
 end
 
